86a04f1f33beeceef3ea96da45096eea999b93ea
yurii <robka4an@gmail.com>
2024-04-13 10:07:32 +0300

    add error message

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,1 +1,9 @@
-export default function ErrorMessage() {}
+import css from "./ErrorMessage.module.css";
+
+export default function ErrorMessage() {
+  return (
+    <div className={css.errorMessage}>
+      <p>Oops, sometsing wents wrong.Please try later</p>
+    </div>
+  );
+}
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,7 +1,6 @@
 import ImageCard from "../ImageCard/ImageCard";
-import css from "./ImageGallery.module.css"
+import css from "./ImageGallery.module.css";
 export default function ImageGallery({ images }) {
-  console.log(images);
   return (
     <ul className={css.imageGalleryList}>
       {images.map((img) => (
